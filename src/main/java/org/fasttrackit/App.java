98acf5f9456ef1eecb0526f5c29ad9812b84c966
Florea Daniel <floreadaniel542@yahoo.com>
2019-08-26 20:41:51 +0300

    Project structure and veribles primiteve data tipse

---
+++ src/main/java/org/fasttrackit/App.java
@@ -0,0 +1,24 @@
+package org.fasttrackit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+
+        // this is a one-line comment
+
+        Car car = new Car();
+        car.name = "Dacia";
+        car.fuelLevel = 60;
+        car.mileage = 8.5;
+        car.doorCount = 5;
+        car.maxSpeed = 180;
+        car.fuelType = "Gasoline";
+
+    }
+
+}
+
---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,14 @@
+package org.fasttrackit;
+
+public class Car {
+
+    String name;
+    double mileage;
+    double maxSpeed;
+    String fuelType;
+    double fuelLevel;
+    double traveleDistance;
+    int doorCount;
+    boolean running;
+
+}
